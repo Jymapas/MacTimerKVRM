a42b6d91aecc22430e1e888c8d696693c2ae1586
Aleksandr Petrakov <Jymapas@yandex.ru>
2025-08-17 23:09:22 +0500

    initial commit

---
+++ TimerKVRM/TimerKVRMApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerKVRMApp.swift
+//  TimerKVRM
+//
+//  Created by Anna Korobkina on 17.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct TimerKVRMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TimerKVRMUITests/TimerKVRMUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  TimerKVRMUITestsLaunchTests.swift
+//  TimerKVRMUITests
+//
+//  Created by Anna Korobkina on 17.08.2025.
+//
+
+import XCTest
+
+final class TimerKVRMUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
